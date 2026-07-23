25e1618ae2bdf1d7f156c463ae1d178b97907d4e
Namchheav009 <namchheavron.info@gmail.com>
2026-07-23 11:06:23 +0700

    initial commit

---
+++ nhamhealth_api/src/main/java/com/nhamhealth/nhamhealth_api/NhamhealthApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhamhealth.nhamhealth_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhamhealthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhamhealthApiApplication.class, args);
+	}
+
+}
