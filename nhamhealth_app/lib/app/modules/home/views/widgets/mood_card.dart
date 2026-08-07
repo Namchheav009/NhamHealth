89c198208d62f3ad2cb15665262d01baac5da9a2
Namchheav009 <namchheavron.info@gmail.com>
2026-08-07 13:16:38 +0700

    fix icon

--- nhamhealth_app/lib/app/modules/home/views/widgets/mood_card.dart
+++ nhamhealth_app/lib/app/modules/home/views/widgets/mood_card.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 import 'inner_shadow.dart';
 
 class MoodCard extends StatelessWidget {
-  final String emoji;
+  final String imageAsset;
   final String label;
   final bool selected;
   final VoidCallback onTap;
 
   const MoodCard({
     super.key,
-    required this.emoji,
+    required this.imageAsset,
     required this.label,
     required this.selected,
     required this.onTap,
@@ -18,8 +18,10 @@ class MoodCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
+    return Semantics(
+      button: true,
+      selected: selected,
+      label: '$label mood',
       child: AnimatedContainer(
         duration: const Duration(milliseconds: 200),
         width: 66,
@@ -27,7 +29,7 @@ class MoodCard extends StatelessWidget {
           color: selected ? const Color(0xFFFFFAFB) : Colors.white,
           borderRadius: BorderRadius.circular(12),
           border: Border.all(
-            color: selected ? const Color(0xFFFFE4E8) : const Color(0xFFF3F3F3),
+            color: selected ? const Color(0xFFFFD7DE) : const Color(0xFFF3F3F3),
           ),
           boxShadow: [
             BoxShadow(
@@ -37,47 +39,65 @@ class MoodCard extends StatelessWidget {
             ),
           ],
         ),
-        child: InnerShadow(
-          borderRadius: BorderRadius.circular(12),
-          shadows: const [
-            BoxShadow(
-              color: Color(0x1000522F),
-              blurRadius: 7,
-              offset: Offset(-1, -1),
-            ),
-          ],
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: FittedBox(
-                    fit: BoxFit.contain,
-                    child: Text(
-                      emoji,
-                      style: const TextStyle(fontSize: 26, height: 1),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 2),
-                SizedBox(
-                  height: 16,
-                  child: FittedBox(
-                    fit: BoxFit.scaleDown,
-                    child: Text(
-                      label,
-                      maxLines: 1,
-                      style: TextStyle(
-                        fontSize: 11,
-                        color: const Color(0xFFFF5265),
-                        fontWeight:
-                            selected ? FontWeight.w600 : FontWeight.w400,
-                      ),
-                    ),
-                  ),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: onTap,
+            borderRadius: BorderRadius.circular(12),
+            child: InnerShadow(
+              borderRadius: BorderRadius.circular(12),
+              shadows: const [
+                BoxShadow(
+                  color: Color(0x1000522F),
+                  blurRadius: 7,
+                  offset: Offset(-1, -1),
                 ),
               ],
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 36,
+                      height: 36,
+                      child: Center(
+                        child: Image.asset(
+                          imageAsset,
+                          width: 36,
+                          height: 36,
+                          fit: BoxFit.contain,
+                          filterQuality: FilterQuality.high,
+                          excludeFromSemantics: true,
+                          errorBuilder:
+                              (_, _, _) => const Icon(
+                                Icons.mood_rounded,
+                                color: Color(0xFFFFB02E),
+                                size: 28,
+                              ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 2),
+                    SizedBox(
+                      height: 16,
+                      child: FittedBox(
+                        fit: BoxFit.scaleDown,
+                        child: Text(
+                          label,
+                          maxLines: 1,
+                          style: TextStyle(
+                            fontSize: 11,
+                            color: const Color(0xFFFF5265),
+                            fontWeight:
+                                selected ? FontWeight.w600 : FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         ),
