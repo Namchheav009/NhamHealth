f835591187e29e07ab512f4d1d98f20fc16e6bfe
Namchheav009 <namchheavron.info@gmail.com>
2026-08-17 17:01:45 +0700

    delete ai chat and fix moods to dynamic

--- nhamhealth_api/src/main/java/com/nhamhealth/nhamhealth_api/repository/MoodRepository.java
+++ nhamhealth_api/src/main/java/com/nhamhealth/nhamhealth_api/repository/MoodRepository.java
@@ -10,5 +10,7 @@ import com.nhamhealth.nhamhealth_api.entity.Mood;
 public interface MoodRepository extends JpaRepository<Mood, Integer> {
     List<Mood> findAllByOrderByMoodNameAsc();
 
+    List<Mood> findAllByIsActiveTrueOrderByMoodNameAsc();
+
     Optional<Mood> findByMoodNameIgnoreCase(String moodName);
 }
