0900d2b51446a5e6edc0667ecd5536b75f6b85db
Rattnakvisal <rattnakvisalchun@gmail.com>
2026-08-18 23:52:57 +0700

    feat : Skeleton

--- nhamhealth_app/lib/app/widgets/page_skeleton.dart
+++ nhamhealth_app/lib/app/widgets/page_skeleton.dart
@@ -1,161 +1,122 @@
 import 'package:flutter/material.dart';
 
-class PageSkeleton extends StatelessWidget {
-  const PageSkeleton.home({super.key}) : _type = _SkeletonType.home;
-  const PageSkeleton.profile({super.key}) : _type = _SkeletonType.profile;
-  const PageSkeleton.wellness({super.key}) : _type = _SkeletonType.wellness;
+import '../theme/app_colors.dart';
 
-  final _SkeletonType _type;
+enum PageSkeletonType { home, profile, wellness }
 
-  @override
-  Widget build(BuildContext context) {
-    final children = switch (_type) {
-      _SkeletonType.home => const [
-        SkeletonAvatarRow(animated: false),
-        SizedBox(height: 16),
-        _SkeletonBox(height: 44, radius: 22),
-        SizedBox(height: 14),
-        SkeletonText(animated: false, lines: 2),
-        SizedBox(height: 14),
-        SkeletonCard(animated: false, mediaHeight: 112),
-        SizedBox(height: 14),
-        SkeletonCard(animated: false, mediaHeight: 160),
-      ],
-      _SkeletonType.profile => const [
-        SkeletonAvatarRow(animated: false, avatarSize: 68),
-        SizedBox(height: 14),
-        SkeletonCard(animated: false, mediaHeight: 90),
-        SizedBox(height: 10),
-        SkeletonText(animated: false, lines: 3),
-        SizedBox(height: 10),
-        SkeletonCard(animated: false, mediaHeight: 100),
-      ],
-      _SkeletonType.wellness => const [
-        SkeletonCard(animated: false, mediaHeight: 170),
-        SizedBox(height: 14),
-        SkeletonCard(animated: false, mediaHeight: 85),
-        SizedBox(height: 14),
-        SkeletonText(animated: false, lines: 4),
-      ],
-    };
-    return _Shimmer(child: Column(children: children));
-  }
-}
-
-enum _SkeletonType { home, profile, wellness }
-
-class SkeletonAvatarRow extends StatelessWidget {
-  const SkeletonAvatarRow({
+class PageSkeleton extends StatefulWidget {
+  const PageSkeleton({
     super.key,
-    this.avatarSize = 52,
-    this.animated = true,
+    required this.type,
+    this.duration = const Duration(milliseconds: 1600),
   });
 
-  final double avatarSize;
-  final bool animated;
+  const PageSkeleton.home({
+    super.key,
+    this.duration = const Duration(milliseconds: 1600),
+  }) : type = PageSkeletonType.home;
+
+  const PageSkeleton.profile({
+    super.key,
+    this.duration = const Duration(milliseconds: 1750),
+  }) : type = PageSkeletonType.profile;
+
+  const PageSkeleton.wellness({
+    super.key,
+    this.duration = const Duration(milliseconds: 1650),
+  }) : type = PageSkeletonType.wellness;
+
+  final PageSkeletonType type;
+  final Duration duration;
 
   @override
-  Widget build(BuildContext context) => _animate(
-    animated,
-    Row(
+  State<PageSkeleton> createState() => _PageSkeletonState();
+}
+
+class _PageSkeletonState extends State<PageSkeleton>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: widget.duration,
+  )..repeat();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Semantics(
+    liveRegion: true,
+    label: 'Loading page content',
+    child: ExcludeSemantics(
+      child: AnimatedBuilder(
+        animation: _controller,
+        child: _layout(),
+        builder:
+            (context, child) => ShaderMask(
+              blendMode: BlendMode.srcATop,
+              shaderCallback: (bounds) {
+                final position = -1.7 + (_controller.value * 3.4);
+                return LinearGradient(
+                  begin: Alignment(position, 0),
+                  end: Alignment(position + .9, 0),
+                  colors: const [
+                    Color(0xFFE6ECE8),
+                    Color(0xFFF9FCFA),
+                    Color(0xFFE6ECE8),
+                  ],
+                  stops: const [0, .5, 1],
+                ).createShader(bounds);
+              },
+              child: child,
+            ),
+      ),
+    ),
+  );
+
+  Widget _layout() => switch (widget.type) {
+    PageSkeletonType.home => const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        _SkeletonBox(
-          width: avatarSize,
-          height: avatarSize,
-          radius: avatarSize / 2,
-        ),
-        const SizedBox(width: 14),
-        const Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              FractionallySizedBox(
-                widthFactor: .72,
-                child: _SkeletonBox(height: 16, radius: 8),
-              ),
-              SizedBox(height: 9),
-              FractionallySizedBox(
-                widthFactor: .45,
-                child: _SkeletonBox(height: 13, radius: 7),
-              ),
-            ],
-          ),
-        ),
+        _AvatarRow(),
+        SizedBox(height: 18),
+        _SkeletonBox(height: 48, radius: 24),
+        SizedBox(height: 18),
+        _TextLines(widths: [.56, .82]),
+        SizedBox(height: 18),
+        _SkeletonCard(height: 150),
+        SizedBox(height: 16),
+        _SkeletonCard(height: 190),
       ],
     ),
-  );
-}
-
-class SkeletonText extends StatelessWidget {
-  const SkeletonText({super.key, this.lines = 4, this.animated = true});
-
-  final int lines;
-  final bool animated;
-
-  @override
-  Widget build(BuildContext context) {
-    const widths = [.66, 1.0, .92, .78, .55];
-    return _animate(
-      animated,
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: List.generate(
-          lines,
-          (index) => Padding(
-            padding: EdgeInsets.only(bottom: index == lines - 1 ? 0 : 9),
-            child: FractionallySizedBox(
-              widthFactor: widths[index % widths.length],
-              child: const _SkeletonBox(height: 14, radius: 7),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class SkeletonCard extends StatelessWidget {
-  const SkeletonCard({super.key, this.mediaHeight = 150, this.animated = true});
-
-  final double mediaHeight;
-  final bool animated;
-
-  @override
-  Widget build(BuildContext context) => _animate(
-    animated,
-    Container(
-      width: double.infinity,
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: const Color(0xFFF2F5F3),
-        borderRadius: BorderRadius.circular(18),
-        border: Border.all(color: const Color(0xFFE0E6E2)),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const FractionallySizedBox(
-            widthFactor: .72,
-            child: _SkeletonBox(height: 16, radius: 8),
-          ),
-          const SizedBox(height: 9),
-          const FractionallySizedBox(
-            widthFactor: .48,
-            child: _SkeletonBox(height: 13, radius: 7),
-          ),
-          const SizedBox(height: 16),
-          _SkeletonBox(height: mediaHeight, radius: 14),
-        ],
-      ),
+    PageSkeletonType.profile => const Column(
+      children: [
+        _ProfileHeaderPlaceholder(),
+        SizedBox(height: 16),
+        _MetricRow(),
+        SizedBox(height: 14),
+        _SkeletonCard(height: 120),
+        SizedBox(height: 14),
+        _SkeletonCard(height: 150),
+      ],
     ),
-  );
+    PageSkeletonType.wellness => const Column(
+      children: [
+        _SkeletonCard(height: 210),
+        SizedBox(height: 16),
+        _SkeletonCard(height: 120),
+        SizedBox(height: 16),
+        _TextLines(widths: [.45, .92, .78]),
+      ],
+    ),
+  };
 }
-
-Widget _animate(bool animated, Widget child) =>
-    animated ? _Shimmer(child: child) : child;
 
 class _SkeletonBox extends StatelessWidget {
-  const _SkeletonBox({this.width, required this.height, this.radius = 16});
+  const _SkeletonBox({this.width, required this.height, this.radius = 14});
+
   final double? width;
   final double height;
   final double radius;
@@ -171,45 +132,106 @@ class _SkeletonBox extends StatelessWidget {
   );
 }
 
-class _Shimmer extends StatefulWidget {
-  const _Shimmer({required this.child});
-  final Widget child;
+class _TextLines extends StatelessWidget {
+  const _TextLines({required this.widths});
+
+  final List<double> widths;
 
   @override
-  State<_Shimmer> createState() => _ShimmerState();
+  Widget build(BuildContext context) => Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      for (var index = 0; index < widths.length; index++) ...[
+        FractionallySizedBox(
+          widthFactor: widths[index],
+          child: const _SkeletonBox(height: 13, radius: 7),
+        ),
+        if (index != widths.length - 1) const SizedBox(height: 10),
+      ],
+    ],
+  );
 }
 
-class _ShimmerState extends State<_Shimmer>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _controller = AnimationController(
-    vsync: this,
-    duration: const Duration(milliseconds: 1200),
-  )..repeat();
+class _AvatarRow extends StatelessWidget {
+  const _AvatarRow();
 
   @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+  Widget build(BuildContext context) => const Row(
+    children: [
+      _SkeletonBox(width: 54, height: 54, radius: 27),
+      SizedBox(width: 14),
+      Expanded(child: _TextLines(widths: [.62, .38])),
+      SizedBox(width: 14),
+      _SkeletonBox(width: 40, height: 40, radius: 20),
+      SizedBox(width: 9),
+      _SkeletonBox(width: 40, height: 40, radius: 20),
+    ],
+  );
+}
+
+class _ProfileHeaderPlaceholder extends StatelessWidget {
+  const _ProfileHeaderPlaceholder();
 
   @override
-  Widget build(BuildContext context) => AnimatedBuilder(
-    animation: _controller,
-    child: widget.child,
-    builder:
-        (_, child) => ShaderMask(
-          blendMode: BlendMode.srcATop,
-          shaderCallback:
-              (bounds) => LinearGradient(
-                begin: Alignment(-1.5 + (_controller.value * 3), 0),
-                end: Alignment(-0.5 + (_controller.value * 3), 0),
-                colors: const [
-                  Color(0xFFE4EAE6),
-                  Color(0xFFF7FAF8),
-                  Color(0xFFE4EAE6),
-                ],
-              ).createShader(bounds),
-          child: child,
+  Widget build(BuildContext context) => const Column(
+    children: [
+      _SkeletonBox(width: 82, height: 82, radius: 41),
+      SizedBox(height: 13),
+      _SkeletonBox(width: 150, height: 17, radius: 9),
+      SizedBox(height: 9),
+      _SkeletonBox(width: 205, height: 12, radius: 6),
+    ],
+  );
+}
+
+class _MetricRow extends StatelessWidget {
+  const _MetricRow();
+
+  @override
+  Widget build(BuildContext context) => const Row(
+    children: [
+      Expanded(child: _SkeletonBox(height: 82, radius: 17)),
+      SizedBox(width: 10),
+      Expanded(child: _SkeletonBox(height: 82, radius: 17)),
+      SizedBox(width: 10),
+      Expanded(child: _SkeletonBox(height: 82, radius: 17)),
+    ],
+  );
+}
+
+class _SkeletonCard extends StatelessWidget {
+  const _SkeletonCard({required this.height});
+
+  final double height;
+
+  @override
+  Widget build(BuildContext context) => Container(
+    width: double.infinity,
+    height: height,
+    padding: const EdgeInsets.all(17),
+    decoration: BoxDecoration(
+      color: AppColors.cardSurface.withValues(alpha: .72),
+      borderRadius: BorderRadius.circular(20),
+      border: Border.all(color: const Color(0xFFE2EAE5)),
+    ),
+    child: const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        FractionallySizedBox(
+          widthFactor: .48,
+          child: _SkeletonBox(height: 15, radius: 8),
         ),
+        SizedBox(height: 11),
+        FractionallySizedBox(
+          widthFactor: .76,
+          child: _SkeletonBox(height: 12, radius: 6),
+        ),
+        Spacer(),
+        FractionallySizedBox(
+          widthFactor: .9,
+          child: _SkeletonBox(height: 38, radius: 13),
+        ),
+      ],
+    ),
   );
 }
