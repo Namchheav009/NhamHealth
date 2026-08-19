35083112765d4457a607a7d89fc6574d1bc70be4
Ron Namchheav <namchheavron.info@gmail.com>
2026-08-19 09:06:08 +0700

    add favorites page

--- nhamhealth_app/lib/app/routes/app_routes.dart
+++ nhamhealth_app/lib/app/routes/app_routes.dart
@@ -10,6 +10,7 @@ abstract class AppRoutes {
   static const String profile = '/profile';
   static const String changePassword = '/profile/change-password';
   static const String notifications = '/notifications';
+  static const String favorites = '/favorites';
 
   static const String wellness = '/wellness';
   static const String calories = '/wellness/calories';
