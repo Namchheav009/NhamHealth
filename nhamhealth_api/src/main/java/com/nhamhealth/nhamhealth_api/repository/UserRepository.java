372843b8b112e0125ad0d7d10f923f9d9780af10
Ron Namchheav <namchheavron.info@gmail.com>
2026-08-23 17:08:53 +0700

    fix admin role

--- nhamhealth_api/src/main/java/com/nhamhealth/nhamhealth_api/repository/UserRepository.java
+++ nhamhealth_api/src/main/java/com/nhamhealth/nhamhealth_api/repository/UserRepository.java
@@ -5,11 +5,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 import com.nhamhealth.nhamhealth_api.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    @EntityGraph(attributePaths = "role")
     Optional<User> findByEmailIgnoreCase(String email);
 
     long countByIsVerifiedTrue();
