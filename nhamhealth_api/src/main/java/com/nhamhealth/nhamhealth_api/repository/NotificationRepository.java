e255e1b86b1a1204d47aebe8ef9b3e47ab06c895
Ron Namchheav <namchheavron.info@gmail.com>
2026-08-25 23:36:15 +0700

    fix dashboard on comment and like post

--- nhamhealth_api/src/main/java/com/nhamhealth/nhamhealth_api/repository/NotificationRepository.java
+++ nhamhealth_api/src/main/java/com/nhamhealth/nhamhealth_api/repository/NotificationRepository.java
@@ -2,6 +2,7 @@ package com.nhamhealth.nhamhealth_api.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,7 @@ import com.nhamhealth.nhamhealth_api.entity.Notification;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+    @EntityGraph(attributePaths = "user")
     List<Notification> findAllByOrderByCreatedAtDesc();
 
     long countByIsReadFalse();
